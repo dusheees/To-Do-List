c39b70beee5d7ea06e31b093f43112e0291dfbd8
Andrey Zhilin <komplayer31@yandex.ru>
2022-07-07 13:26:58 +0900

    Initial Commit

---
+++ To Do List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To Do List
+//
+//  Created by Андрей on 07.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
